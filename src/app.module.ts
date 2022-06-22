d1440009f4a80b2cfe45bab7c6da2d455dca9e4f
satou-yosiyuki <stuysyk1027@icloud.com>
2022-06-22 23:46:45 +0900

    マイグレーション

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+TypeOrmModule;
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
