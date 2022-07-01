2f8fcc17d7ca524d616e66a745252ab98c6cfc65
satou-yosiyuki <stuysyk1027@icloud.com>
2022-07-01 23:47:46 +0900

    ログイン機能

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserRepository } from './use.repository';
-
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository])],
+  imports: [
+    TypeOrmModule.forFeature([UserRepository]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'secretkey123',
+      signOptions: {
+        expiresIn: 3600,
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
